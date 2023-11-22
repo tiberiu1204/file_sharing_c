4df5c0b58d014e543152a84cfc533fc7743b2884
Tiberiu Popescu <tiberiu1204@gmail.com>
2023-11-22 15:11:13 +0200

    Added server-side multithreading

--- include/server.h
+++ include/server.h
@@ -1,5 +1,13 @@
 #ifndef SERVER_H
 #define SERVER_H
+#include <netinet/in.h>
+
+typedef struct service_client_args
+{
+	int sockfd;
+	struct sockaddr_in addr;
+
+} service_client_args;
 
 int start_server(int port);
 
--- src/server/server.c
+++ src/server/server.c
@@ -5,6 +5,8 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
 
 void error(const char* msg)
 {
@@ -12,30 +14,59 @@ void error(const char* msg)
 	exit(1);
 }
 
+void *service_client(void *args)
+{
+	service_client_args *arguments = (service_client_args *) args;
+	int sockfd = arguments->sockfd;
+	struct sockaddr_in client_addr = arguments->addr;
+	char buffer[256];
+	memset(buffer, 0, 255);
+	int n = read(sockfd, buffer, 255);
+	const char client_ip[INET_ADDRSTRLEN];
+	unsigned short port = ntohs(client_addr.sin_port);
+	inet_ntop(AF_INET, &client_addr.sin_addr, (char *) client_ip, INET_ADDRSTRLEN);
+	if(n < 0)
+	{
+		fprintf(stderr, "[ERROR] could not read from socket with address %s\n", (char *) client_ip);
+		return NULL;
+	}
+	printf("[%s:%hu] %s\n", (char *) client_ip, port, (char *) buffer);
+	close(sockfd);
+	return NULL;
+}
+
 int start_server(int port)
 {
-	int sockfd, newsockfd, n;
+	int sockfd, newsockfd;
 	socklen_t clilen;
-	char buffer[256];
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in server_addr, client_addr;
 	if(sockfd < 0)
-		error("[ERROR] could not open socket");
+		error("[ERROR] could not open socket\n");
 	memset((char *) &server_addr, 0, sizeof(server_addr));		
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(port);
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	if(bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0)
-		error("[ERROR] could not bind socket");
+		error("[ERROR] could not bind socket\n");
 	listen(sockfd, 5);
 	clilen = sizeof(client_addr);
-	newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &clilen);
-	if(newsockfd < 0)
-		error("[ERROR] could not accept connection");
-	memset(buffer, 0, 255);
-	n = read(newsockfd, buffer, 255);
-	if(n < 0) error("[ERROR] error while reading from socket");
-	printf("Here is the message %s", buffer);
+	while(1)
+	{
+		newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &clilen);
+		if(newsockfd < 0)
+		{
+			perror("[ERROR] could not accept connection\n");
+			continue;
+		}
+		service_client_args args;
+		memset(&args, 0, sizeof(args));
+		args.sockfd = newsockfd;
+		args.addr = client_addr;
+		pthread_t thread_id;
+		pthread_create(&thread_id, NULL, service_client, (void *) &args);
+	}
+	close(sockfd);
 	return 0;
 }
 
